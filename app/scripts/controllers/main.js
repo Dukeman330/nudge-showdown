1af1f78da6cfb8903c6a1b1ac494e207e5a7f49f
Chris Garson <cgarson@gmail.com>
2015-04-29 18:41:57 -0400

    scaffolded project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nudgeShowdownApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nudgeShowdownApp
+ */
+angular.module('nudgeShowdownApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
