616c43884bd1d69dbb2eb07a5b18a54d3a011773
Chris Garson <cgarson@gmail.com>
2015-05-20 22:23:34 -0400

    implemented date picker

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,9 +10,29 @@
 angular.module('nudgeShowdownApp')
   .controller('MainCtrl', ['$scope', 'leaderboard',
               function ($scope, leaderboard) {
-    leaderboard.get().then(function(response) {
-      $scope.startDate = response.data.start_date;
-      $scope.endDate = response.data.end_date;
-      $scope.companies = response.data.companies;
-    });
+    $scope.now = true;
+
+    var getData = function(startDate) {
+      leaderboard.get(startDate).then(function(response) {
+        $scope.startDate = response.data.start_date;
+        $scope.endDate = response.data.end_date;
+        $scope.companies = response.data.companies;
+        $scope.now = parseInt(moment().format('X'), 10) < $scope.endDate;
+      });
+    };
+
+    getData();
+
+    $scope.changeDate = function(goBack) {
+      if (goBack || !$scope.now) {
+        var newDate = moment($scope.startDate*1000);
+        if (goBack) {
+          newDate = newDate.subtract(7, 'days');
+        } else {
+          newDate = newDate.add(7, 'days');
+        }
+
+        getData(newDate);
+      }
+    };
   }]);
--- app/scripts/services/leaderboard.js
+++ app/scripts/services/leaderboard.js
@@ -3,8 +3,13 @@
 angular.module('nudgeShowdownApp')
   .factory('leaderboard', ['$http', function($http) {
     return {
-      get: function() {
-        return $http.get('https://api.nudgeyourself.com/showdown');
+      get: function(startDate) {
+        var url = 'http://nudge-api.app:8000/showdown';
+        if (startDate) {
+          url += '?date='+startDate.format('YY-MM-DD');
+        }
+
+        return $http.get(url);
       }
     };
   }]);
